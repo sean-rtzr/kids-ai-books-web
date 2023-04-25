72f3c455159e5562424c22f4c913a7602027bcd8
sean-rtzr <sean@rtzr.ai>
2023-04-26 02:32:54 +0900

    Changed server port.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	server: {
+		port: 32720,
+	},
 	css: [
 		"vuetify/lib/styles/main.sass",
 		"@mdi/font/css/materialdesignicons.min.css",
